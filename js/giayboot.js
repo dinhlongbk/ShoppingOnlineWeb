0d95cfaf202db8c6c21e2d438c243a4304ff6950
khoajunior <khoajunior666@gmail.com>
2019-03-04 18:41:08 +0700

    update giayboot.js anh img

--- js/giayboot.js
+++ js/giayboot.js
@@ -19,8 +19,43 @@ let products = {
         {maSanPham: "1116", tenSanPham: "bootwomen4545",giaTien: 245000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/bootwomen4545.png"},
         {maSanPham: "1116", tenSanPham: "bootwomen4544",giaTien: 245000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/bootwomen4544.png"},
     ],
-    "sport": [],
-    "sandal": []
+    "sport": [
+         {maSanPham: "2111", tenSanPham: "Giày Thể Thao TT- 3236",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3236.png"},
+         {maSanPham: "2112", tenSanPham: "Giày Thể Thao TT- 3235",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3235.png"},
+         {maSanPham: "2113", tenSanPham: "Giày Thể Thao TT- 3234",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3234.png"},
+         {maSanPham: "2114", tenSanPham: "Giày Thể Thao TT- 3233",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3233.png"},
+         {maSanPham: "2115", tenSanPham: "Giày Thể Thao TT- 3232",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3232.png"},
+         {maSanPham: "2116", tenSanPham: "Giày Thể Thao TT- 3231",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3231.png"},
+         {maSanPham: "2117", tenSanPham: "Giày Thể Thao TT- 3230",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3230.png"},
+         {maSanPham: "2118", tenSanPham: "Giày Thể Thao TT- 3229",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3229.png"},
+         {maSanPham: "2119", tenSanPham: "Giày Thể Thao TT- 3228",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3228.png"},
+         {maSanPham: "2120", tenSanPham: "Giày Thể Thao TT- 3227",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3227.png"},
+         {maSanPham: "2121", tenSanPham: "Giày Thể Thao TT- 3226",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3226.png"},
+         {maSanPham: "2122", tenSanPham: "Giày Thể Thao TT- 3225",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3225.png"},
+         {maSanPham: "2123", tenSanPham: "Giày Thể Thao TT- 3224",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3224.png"},
+         {maSanPham: "2124", tenSanPham: "Giày Thể Thao TT- 3223",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3223.png"},
+         {maSanPham: "2125", tenSanPham: "Giày Thể Thao TT- 3222",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3222.png"},
+         {maSanPham: "2126", tenSanPham: "Giày Thể Thao TT- 3221",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sport/Giày Thể Thao TT- 3221.png"},
+    ],
+    "sandal": [
+         {maSanPham: "3111", tenSanPham: "Giày Sandal nữ SD - 2638",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2638.png"},
+         {maSanPham: "3112", tenSanPham: "Giày Sandal nữ SD - 2637",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2637.png"},
+         {maSanPham: "3113", tenSanPham: "Giày Sandal nữ SD - 2636",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2636.png"},
+         {maSanPham: "3114", tenSanPham: "Giày Sandal nữ SD - 2635",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2635.png"},
+         {maSanPham: "3115", tenSanPham: "Giày Sandal nữ SD - 2634",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2634.png"},
+         {maSanPham: "3116", tenSanPham: "Giày Sandal nữ SD - 2633",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2633.png"},
+         {maSanPham: "3117", tenSanPham: "Giày Sandal nữ SD - 2632",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2632.png"},
+         {maSanPham: "3118", tenSanPham: "Giày Sandal nữ SD - 2631",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2631.png"},
+         {maSanPham: "3119", tenSanPham: "Giày Sandal nữ SD - 2630",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2630.png"},
+         {maSanPham: "3120", tenSanPham: "Giày Sandal nữ SD - 2629",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2629.png"},
+         {maSanPham: "3121", tenSanPham: "Giày Sandal nữ SD - 2628",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2628.png"},
+         {maSanPham: "3122", tenSanPham: "Giày Sandal nữ SD - 2627",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2627.png"},
+         {maSanPham: "3123", tenSanPham: "Giày Sandal nữ SD - 2626",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2626.png"},
+         {maSanPham: "3124", tenSanPham: "Giày Sandal nữ SD - 2625",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2625.png"},
+         {maSanPham: "3125", tenSanPham: "Giày Sandal nữ SD - 2624",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2624.png"},
+         {maSanPham: "3126", tenSanPham: "Giày Sandal nữ SD - 2623",giaTien: 250000, mauSac: "đen", gioiTinh: "nữ", hinhAnh: "../img/sandal/Giày Sandal nữ SD - 2623.png"},
+
+    ]
 };
 
 //========== PAGER OBJECT =========//
